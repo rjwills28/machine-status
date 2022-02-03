2a944b31710eb40d6977f754e13228856be893cf
Rebecca Williams <rjw@observatorysciences.co.uk>
2022-02-03 12:44:41 +0000

    Adding image to header for the web-app version.

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -3,6 +3,9 @@ import classes from "./header.module.css";
 export const Header = (): JSX.Element => {
   return (
     <header className={classes.header}>
+      <div >
+        <img src="/img/Diamond_logo_col.jpg" className={classes.img}/>
+      </div>
       <h1 className={classes.h1}>
         <a href="/">Machine Status</a>
       </h1>
