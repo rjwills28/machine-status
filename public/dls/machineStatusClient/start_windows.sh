396168975aef3fdf8b9d79efbd01e1dfc78d6930
Andy Wilson <andrew.wilson@diamond.ac.uk>
2022-02-22 15:35:27 +0000

    Remove unused files
    Remove references to logo images which are not visible.
    Remove the images themselves.
    Remove unused opi files and shell scripts.
    
    Fixes #4

--- public/dls/machineStatusClient/start_windows.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/env bash
-
-DIR=$(dirname $0)
-cd $DIR
-EDMCOLORFILE=colours.list edm -x Pday.edl Pweek.edl Pmessage.edl FE1.edl FE2.edl
--- public/dls/machineStatusClient/status_reboot.sh
+++
@@ -1,6 +0,0 @@
-while [ 1 ]; do
-    if [ $(caget -t CS-CS-MSTAT-01:REBOOT) -eq 1 ]; then
-        reboot
-    fi
-    sleep 10
-done
