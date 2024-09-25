d4cc31bba2ad538eede3d80ad51bb633e5778e2e
Rebecca Williams <rjw@observatorysciences.co.uk>
2024-09-25 12:34:19 +0100

    Stick with VITE env variables

--- vite.config.ts
+++ vite.config.ts
@@ -17,15 +17,6 @@ export default defineConfig(({ command, mode }) => {
             open: true,
             // this sets a default port to 3000  
             port: 3000, 
-        },
-        define: {
-            'process.env.REACT_APP_CONIQL_SOCKET': JSON.stringify(env.REACT_APP_CONIQL_SOCKET),
-            'process.env.REACT_APP_CONIQL_SSL': JSON.stringify(env.REACT_APP_CONIQL_SSL),
-            'process.env.VITE_PROFILER_ENABLED': JSON.stringify(env.VITE_PROFILER_ENABLED),
-            'process.env.VITE_THROTTLE_PERIOD': JSON.stringify(env.VITE_THROTTLE_PERIOD),
-            'process.env.VITE_LOG_LEVEL': JSON.stringify(env.VITE_LOG_LEVEL),
-            'process.env.VITE_CYCLE_CONFIG_FILE': JSON.stringify(env.VITE_CYCLE_CONFIG_FILE),
-            'process.env.VITE_BUILD_TARGET': JSON.stringify(env.VITE_BUILD_TARGET),
-        },
+        }
     }    
 })
